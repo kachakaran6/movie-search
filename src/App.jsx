deb53bcefa2893cbb11384404b9bd1cb985e8fb9
kachakaran6 <kachakaran06@gmail.com>
2025-02-22 23:37:50 +0530

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import SearchBar from "./components/SearchBar";
 
 function App() {
   const [allMovieData, setAllMovieData] = useState([]);
-  const [searchMovie] = useState("");
+  const [searchMovie, setSearchMovie] = useState("");
   const [loading, setLoading] = useState(false);
   // const api = process.env.REACT_APP_API;
 
   const fetchMovieData = async () => {
     try {
       setLoading(true);
-      const res = await fetch("");
+      const res = await fetch(
+        "http://www.omdbapi.com/?i=tt3896198&apikey=bda7ec7f"
+      );
       const data = await res.json();
-      setAllMovieData(data.Search);
+      setAllMovieData(data);
       console.log(data);
 
       setLoading(false);
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-const SearchBar = (searchMovie, setSearchMovie, fetchMovieData) => {
+function SearchBar(searchMovie, setSearchMovie, fetchMovieData) {
   return (
     <div className="flex justify-center mt-8">
       <div className="relative w-full max-w-md">
@@ -7,31 +7,20 @@ const SearchBar = (searchMovie, setSearchMovie, fetchMovieData) => {
           type="text"
           placeholder="Search..."
           value={searchMovie}
-          onChange={(e) => searchMovie(e.target.value)}
+          onChange={(e) => setSearchMovie(e.target.value)} // Fixed to set state
           className="w-full py-2 px-4 rounded-lg border border-[#BB95FD] focus:outline-none focus:ring-2 focus:ring-[#83F8E8] placeholder-[#BB95FD] text-gray-800"
         />
 
-        {/* <!-- Search Icon --> */}
-        <div className="absolute inset-y-0 right-0 flex items-center pr-3">
-          <svg
-            onClick={fetchMovieData}
-            className="w-5 h-5 text-[#BB95FD]"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M11 4a7 7 0 11-7 7 7 7 0 017-7zm0 14a7 7 0 007-7 7 7 0 00-7 7z"
-            ></path>
-          </svg>
-        </div>
+        {/* <!-- Search Button --> */}
+        <button
+          onClick={fetchMovieData}
+          className="absolute inset-y-0 right-0 flex items-center pr-3 bg-[#BB95FD] text-white rounded-lg px-4 py-2"
+        >
+          Search
+        </button>
       </div>
     </div>
   );
-};
+}
 
 export default SearchBar;
